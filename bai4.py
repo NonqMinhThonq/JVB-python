007e06832d6d235ad50a9f7ad452f6f33104d62e
Thonq <notho1002@gmail.com>
2024-11-29 11:24:19 +0700

    Commit changes for bai4.py and output.xlsx

--- bai4.py
+++ bai4.py
@@ -1,6 +1,6 @@
 def dao_nguoc_cac_tu(chuoi):
-    tu_dao_nguoc = chuoi.split()[::-1]  # Tách chuỗi thành các từ và đảo ngược thứ tự từ
-    return " ".join(tu_dao_nguoc)       # Ghép lại các từ đã đảo ngược thành chuỗi
+    tu_dao_nguoc = chuoi.split()[::-1] 
+    return " ".join(tu_dao_nguoc)      
 
 chuoi = input("Nhập chuỗi: ")
 print("Chuỗi đảo ngược là:", dao_nguoc_cac_tu(chuoi))
